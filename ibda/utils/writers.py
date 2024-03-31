9c54b85f25e6b9c25a6147235a64235bf0695ddd
MurphLaws <lasso.nicolas@correounivalle.edu.co>
2024-03-31 00:38:50 -0500

    Added new script with the class Attack for performing attacks more easily. Solving many problems on the drawing od bases amd targets, and savings. Also saving all the necesarry things for computing Influence. Still need to create the main function using click

--- ibda/utils/writers.py
+++ ibda/utils/writers.py
@@ -4,10 +4,10 @@ from pathlib import Path
 import numpy as np
 
 
-def save_as_json(data, savedir, fname):
+def save_as_json(data, savedir, fname, indent = None):
     Path(savedir).mkdir(parents=True, exist_ok=True)
     with open(Path(savedir, fname), "w") as f:
-        json.dump(data, f)
+        json.dump(data, f, indent=indent)
 
 
 def save_as_np(data, savedir, fname):
