8e6ee47450d9cae5b3e41e95113eae0182d61c5f
MurphLaws <lasso.nicolas@correounivalle.edu.co>
2024-04-03 17:01:48 -0500

    Finished poison attack pipeline

---
+++ poison_pipeline_runner.py
@@ -0,0 +1,37 @@
+
+#run_poison_attacks:
+#	python -m attack_generation.poisons.run_attacks \
+#	--data_name $(DATA_NAME) \
+#	--train_data_fp data/clean/$(DATA_NAME)/$(SUBSET_FOLDER)/train.pt \
+#	--test_data_fp data/clean/$(DATA_NAME)/$(SUBSET_FOLDER)/test.pt \
+#	--model_conf_fp configs/resnet/resnet_$(DATA_NAME).json \
+#	--dir_suffix $(SUBSET_FOLDER) \
+#	$(if $(CKPT_NUMBER),--model_ckpt_fp "results/resnet20/$(DATA_NAME)/$(SUBSET_FOLDER)/clean/ckpts/checkpoint-$(CKPT_NUMBER).pt") \
+#	$(if $(SEED),--seed $(SEED),--seed 0) \
+#	$(if $(DEVICE),--device $(DEVICE),--device cpu) \
+#	$(if $(NUM_POISONS),--num_poisons $(NUM_POISONS),--num_poisons 1) \
+#	$(if $(NUM_TARGETS),--num_targets $(NUM_TARGETS),--num_targets 2) \
+#	$(if $(MAX_ITER),--max_iter $(MAX_ITER),--max_iter 3)
+
+import os
+
+import click
+
+SEEDS = [0, 1, 2, 3, 4]
+
+
+@click.command()
+@click.option("--data", required=True, type=click.Choice(['mnist', 'cifar10', 'fmnist']))
+@click.option("--device", required=True, type=click.STRING)
+def run_command(data: str, device: str):
+	for seed in SEEDS:
+			commands = (
+				#f"make prepare_data SEED={seed};"
+				f"make run_poison_attacks DATA_NAME={data} SUBSET_FOLDER=subset_id{seed}_r0.1 DEVICE={device} NUM_POISONS=10 NUM_TARGETS=10 MAX_ITER=30 SEED={seed};"
+			)
+			os.system(commands)
+
+
+if __name__ == '__main__':
+	run_command()
+	pass
